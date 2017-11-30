ede7801f564311f6c3db739c52f19e8dee26547e
ctmakro <ctmakro@gmail.com>
2017-12-01 04:50:52 +0800

    different build script on win and linux

--- build_for_all_platforms.bat
+++ build_for_all_platforms.bat
@@ -1,4 +1,9 @@
-GOARCH=amd64 GOOS=windows go build -o went_win64.exe
-GOARCH=amd64 GOOS=darwin go build -o went_darwin
-GOARCH=amd64 GOOS=linux go build -o went_linux64
-tar -czf went.tar.gz went_linux64 went_darwin went_win64.exe 
+set GOARCH=amd64
+set GOOS=windows
+go build -o went_win64.exe
+
+set GOOS=darwin
+go build -o went_darwin
+
+set GOOS=linux
+go build -o went_linux64
---
+++ build_for_all_platforms.sh
@@ -0,0 +1,4 @@
+GOARCH=amd64 GOOS=windows go build -o went_win64.exe
+GOARCH=amd64 GOOS=darwin go build -o went_darwin
+GOARCH=amd64 GOOS=linux go build -o went_linux64
+tar -czf went.tar.gz went_linux64 went_darwin went_win64.exe
